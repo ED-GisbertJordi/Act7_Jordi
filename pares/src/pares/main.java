2d751189b131dd70d0ba6ec5ee90250ba0a991fa
Jordi Gisbert <jota.gvg@gmail.com>
2023-12-30 20:42:27 +0100

    Sobrecarga Metodo imprimirNumeros

--- pares/src/pares/main.java
+++ pares/src/pares/main.java
@@ -3,10 +3,16 @@ package pares;
 public class main {
     public static void main(String[] args) {
     	imprimirNumeros(2, 20);
+    	imprimirNumeros(2, 2, 20);
     }
     private static void imprimirNumeros(int numeroIncrementativo, int max) {
     	int principio = 0;
     	for(int i = principio; i<=max; i+=numeroIncrementativo) {
+    		System.out.println(i);
+    	}
+    }
+    private static void imprimirNumeros(int numePrincipio, int numeroIncrementativo, int max) {
+    	for(int i = numePrincipio; i<=max; i+=numeroIncrementativo) {
     		System.out.println(i);
     	}
     }
