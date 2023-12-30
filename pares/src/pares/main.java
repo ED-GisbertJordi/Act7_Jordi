f5340f9b3008baad57306c1b746fccc5aa24248f
Jordi Gisbert <jota.gvg@gmail.com>
2023-12-30 20:21:51 +0100

    Segundo Commit: Cambio, añadido metido imprimirNumeros, para sacar la función del main

--- pares/src/pares/main.java
+++ pares/src/pares/main.java
@@ -2,8 +2,12 @@ package pares;
 
 public class main {
     public static void main(String[] args) {
-    	for(int i = 0; i<=20; i+=2) {
+    	imprimirNumeros(2, 20);
+    }
+    private static void imprimirNumeros(int numeroIncrementativo, int max) {
+    	int principio = 0;
+    	for(int i = principio; i<=max; i+=numeroIncrementativo) {
     		System.out.println(i);
-    	}	
+    	}
     }
 }
